69df1e3eecab1bf19753de082ffac2fca99eded8
吴丹丹 <danw@kailuzhe.cn>
2016-07-05 15:29:50 +0800

    连接mysql测试

--- app/modules/core/db.go
+++ app/modules/core/db.go
@@ -1,14 +1,13 @@
 package core
 
 import (
-    "github.com/astaxie/beego/orm"
-    "github.com/astaxie/beego"
-    _ "github.com/go-sql-driver/mysql" // import your used driver
-    "fmt"
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql" // import your used driver
 )
+
 func init() {
-    // set default database
-    // orm.RegisterDataBase("default", "mysql", "root:root@/my_db?charset=utf8", 30)
-    fmt.Println(beego.AppConfig.String("db_user")+":"+beego.AppConfig.String("db_pass")+"@/"+beego.AppConfig.String("db_name")+"?charset=utf8")
-    orm.RegisterDataBase("default", "mysql", beego.AppConfig.String("db_user")+":"+beego.AppConfig.String("db_pass")+"@/"+beego.AppConfig.String("db_name")+"?charset=utf8", 30)
+	// set default database
+	// orm.RegisterDataBase("default", "mysql", "root:root@/mybeegodemo?charset=utf8", 30)
+	orm.RegisterDataBase("default", "mysql", beego.AppConfig.String("db_user")+":"+beego.AppConfig.String("db_pass")+"@/"+beego.AppConfig.String("db_name")+"?charset=utf8", 30)
 }
