3313c6d60bae85bbed6ad002e7360f3015964df3
吴丹丹 <danw@kailuzhe.cn>
2016-07-05 16:21:09 +0800

    postgresql连接测试

--- app/modules/core/db.go
+++ app/modules/core/db.go
@@ -1,12 +1,23 @@
 package core
 
 import (
+	/*"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql" // import your used driver*/
+
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
-	_ "github.com/go-sql-driver/mysql" // import your used driver
+	//  "postgresql/models"
+
+	_ "github.com/lib/pq"
+	//  _ "postgresql/routers"
 )
 
 func init() {
+	// PostgreSQL 配置
+	// orm.RegisterDriver("postgres", orm.DR_Postgres) // 注册驱动
+	orm.RegisterDataBase("default", "postgres", "user="+beego.AppConfig.String("db_user")+" password="+beego.AppConfig.String("db_pass")+" dbname="+beego.AppConfig.String("db_name")+" host=127.0.0.1 port=5432 sslmode=disable")
+	//mysql 配置
 	// set default database
 	// orm.RegisterDataBase("default", "mysql", "root:root@/mybeegodemo?charset=utf8", 30)
 	orm.RegisterDataBase("default", "mysql", beego.AppConfig.String("db_user")+":"+beego.AppConfig.String("db_pass")+"@/"+beego.AppConfig.String("db_name")+"?charset=utf8", 30)
